5fafb24e7b2bc147853b1c163eb9689228601fe1
Kroy <gabanyikarolyarpad@gmail.com>
2015-11-27 14:31:58 +0100

    DiagramAction feltöltve

---
+++ SzarHF_umlweb/src/com/szar/umlweb/shared/DiagramAction.java
@@ -0,0 +1,12 @@
+package com.szar.umlweb.shared;
+
+public class DiagramAction extends DiagramState {
+
+	public DiagramAction(String string) {
+		super(string);
+		this.widgetType=WidgetType.ACTION;
+	}
+	
+//	super
+
+}
